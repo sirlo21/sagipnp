05f1812627a9d7b4115814618f1bf56146222804
Ruben <ruben.ignaciogz@gmail.com>
2015-09-21 01:42:40 -0500

    change js

--- statics/js/reparar.js
+++ statics/js/reparar.js
@@ -1,45 +1,41 @@
-function numero_unidad_medida(form){
-	$("input").change(function(ev){
-		var id = $(this).attr("id");
-		if(typeof(id) == "string"){
-			if(id.endsWith(form+"_numero_unidad_medida")){
-				var input = $(this);
-				return Number(input.val());
-			}
-			else
-				return false;
-		}
+function numero_unidad_medida(input,form){
+	var unidad = input.attr("id").replace(form+"_precio_unitario",form+"_numero_unidad_medida");
+	unidad = unidad.replace(form+"_precio_total_referencial",form+"_numero_unidad_medida");
+	var id = $("#"+unidad).attr("id");
+	if(typeof(id) == "string"){
+		if(id.endsWith(form+"_numero_unidad_medida"))
+			return Number($("#"+id).val());
 		else
 			return false;
-	});
+	}
+	else
+		return false;
 }
-function precio_unitario(form){
-	$("input").change(function(ev){
-		var id = $(this).attr("id");
-		if(typeof(id) == "string"){
-			if(id.endsWith(form+"_precio_unitario")){
-				var input = $(this);
-				return Number(input.val());
-			}
-			else
-				return false;
-		}
+function precio_unitario(input,form){
+	var precio = input.attr("id").replace(form+"_numero_unidad_medida",form+"_precio_unitario");
+	precio = precio.replace(form+"_precio_total_referencial",form+"_precio_unitario");
+	var id = $("#"+precio).attr("id");
+	if(typeof(id) == "string"){
+		if(id.endsWith(form+"_precio_unitario"))
+			return Number($("#"+id).val());
 		else
 			return false;
-	});
+	}
+	else
+		return false;
 }
 function precio_total_referencial(form){
 	$("input").change(function(ev){
-		var numero_unidad_medida = numero_unidad_medida(form);
-		var precio_unitario = precio_unitario(form);
+		var $numero_unidad_medida = numero_unidad_medida($(this),form);
+		var $precio_unitario = precio_unitario($(this),form);
 		var id = $(this).attr("id");
 		if(typeof(id) == "string"){
-			if(numero_unidad_medida && precio_unitario){
+			if($numero_unidad_medida && $precio_unitario){
 				var input = $(this);
 				$("input").each(function(index){
 					var input_id = $(this).attr("id");
-					if($input.attr("id") == input_id){
-						var total = numero_unidad_medida * precio_unitario;
+					if(input.attr("id") == input_id){
+						var total = $numero_unidad_medida*$precio_unitario;
 						if(id.endsWith(form+"_precio_unitario"))
 							var precio_total = input_id.replace(form+"_precio_unitario",form+"_precio_total_referencial");
 						else if(id.endsWith(form+"_numero_unidad_medida"))
