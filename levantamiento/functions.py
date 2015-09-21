afe1f287ef29cbf827dc7a6cf7b16ff343f13d1c
Ruben <ruben.ignaciogz@gmail.com>
2015-09-21 02:52:12 -0500

    remove all

--- levantamiento/functions.py
+++
@@ -1,7 +0,0 @@
-from ayudas.models import Ayuda
-
-def posiciones_de_ayuda(form_ayuda):
-	posicion_ayuda = []
-	for ayuda in Ayuda.objects.filter(form=form_ayuda):
-		posicion_ayuda.append(ayuda.posicion)
-	return posicion_ayuda
--- statics/js/select_ubigeo.js
+++
@@ -1,8 +0,0 @@
-var getProvincias = getProvinciasFactory("#id_ubigeo_1", "#id_ubigeo_2");
-var getDistritos = getDistritosFactory("#id_ubigeo_2");
-$('#id_ubigeo_0').on('change', function (){
-	getProvincias(this.value);
-});
-$('#id_ubigeo_1').on('change', function (){
-	getDistritos(this.value);
-});
