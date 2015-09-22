92982b430aaa85bd57fdd69d48984f31c6a54019
Ruben <ruben.ignaciogz@gmail.com>
2015-09-22 08:49:57 -0500

    change views.py

--- metrados/views.py
+++ metrados/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from levantamiento.models import Levantamiento,RegionPolicial,TComisaria,CComisaria,Especialidad,Category
+from reparar.models import Techo,InstalacionSanitaria,InstalacionElectrica,MurosParedes
+from ayudas.models import Ayuda
+from django.contrib.auth.decorators import login_required
+from levantamiento.functions import posiciones_de_ayuda
+from django.http import JsonResponse
+from ubigeo.models import Ubigeo
 
-# Create your views here.
+# @login_required
+def ficha_tecnica(request,id):
+	levantamiento = Levantamiento.objects.get(id=id)
+	distrito = levantamiento.ubigeo
+	provincia = Ubigeo.objects.get(id=distrito.parent_id)
+	departamento = Ubigeo.objects.get(id=provincia.parent_id)
+	context = {"obj": levantamiento,"departamento": departamento,"provincia": provincia,"distrito": distrito}
+	return render(request,"metrados/ficha_tecnica.html",context)
