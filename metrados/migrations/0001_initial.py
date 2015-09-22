7849f046475f0b8e3c2c81da1c305278cd982dcd
Ruben <ruben.ignaciogz@gmail.com>
2015-09-22 13:08:20 -0500

    add 0001_initial.py

--- metrados/migrations/0001_initial.py
+++ metrados/migrations/0001_initial.py
@@ -11,6 +11,23 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='FichaTecnica',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=500)),
+                ('numero', models.IntegerField(default=0)),
+                ('largo', models.FloatField(default=0)),
+                ('ancho', models.FloatField(default=0)),
+                ('alto', models.FloatField(default=0)),
+                ('parcial', models.IntegerField(default=0)),
+                ('unidad', models.IntegerField(default=0)),
+                ('punitario', models.IntegerField(default=0)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Metrado1',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -56,5 +73,29 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='fichatecnica',
+            name='metrado1',
+            field=models.ForeignKey(related_name='ficha_tecnica', to='metrados.Metrado1'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='fichatecnica',
+            name='metrado2',
+            field=models.ForeignKey(related_name='ficha_tecnica', to='metrados.Metrado2'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='fichatecnica',
+            name='metrado3',
+            field=models.ForeignKey(related_name='ficha_tecnica', blank=True, to='metrados.Metrado3', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='fichatecnica',
+            name='metrado4',
+            field=models.ForeignKey(related_name='ficha_tecnica', blank=True, to='metrados.Metrado4', null=True),
+            preserve_default=True,
         ),
     ]
