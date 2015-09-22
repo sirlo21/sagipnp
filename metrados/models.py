5f5560fe7335efae7e3934c8095e4055de339718
Ruben <ruben.ignaciogz@gmail.com>
2015-09-22 13:09:06 -0500

    change models.py

--- metrados/models.py
+++ metrados/models.py
@@ -4,17 +4,43 @@ class Metrado1(models.Model):
 	codigo = models.CharField(max_length=25)
 	descripcion = models.TextField()
 
+	def __unicode__(self):
+		return self.codigo
+
 class Metrado2(models.Model):
 	codigo = models.CharField(max_length=25)
 	descripcion = models.TextField()
 	metrado1_id = models.ForeignKey(Metrado1,related_name='metrado_2')
+
+	def __unicode__(self):
+		return self.codigo
 
 class Metrado3(models.Model):
 	codigo = models.CharField(max_length=25)
 	descripcion = models.TextField()
 	metrado2_id = models.ForeignKey(Metrado2,related_name='metrado_3')
 
+	def __unicode__(self):
+		return self.codigo
+
 class Metrado4(models.Model):
 	codigo = models.CharField(max_length=25)
 	descripcion = models.TextField()
 	metrado3_id = models.ForeignKey(Metrado3,related_name='metrado_4')
+
+	def __unicode__(self):
+		return self.codigo
+
+class FichaTecnica(models.Model):
+	nombre = models.CharField(max_length=500)
+	metrado1 = models.ForeignKey(Metrado1,related_name='ficha_tecnica')
+	metrado2 = models.ForeignKey(Metrado2,related_name='ficha_tecnica')
+	metrado3 = models.ForeignKey(Metrado3,related_name='ficha_tecnica',null=True,blank=True)
+	metrado4 = models.ForeignKey(Metrado4,related_name='ficha_tecnica',null=True,blank=True)
+	numero = models.IntegerField(default=0)
+	largo = models.FloatField(default=0)
+	ancho = models.FloatField(default=0)
+	alto = models.FloatField(default=0)
+	parcial = models.IntegerField(default=0)
+	unidad = models.IntegerField(default=0)
+	punitario = models.IntegerField(default=0)
