e4b4ffd2f92493798dac92ecf6d38818be9250f6
Ruben <ruben.ignaciogz@gmail.com>
2015-09-24 01:04:50 -0500

    change metrados.js

--- statics/js/metrados.js
+++ statics/js/metrados.js
@@ -1,118 +1,117 @@
 $(document).ready(function(){
-	$("#submit").hide();
-	$("#id_metrado1").change(function(ev){
-		var url = "/metrado/?metrado1_id="+$(this).val();
-		$.getJSON(url,function(data){
-			$("#id_metrado2").children("option[value]").remove();
-			$("#id_metrado3").children("option[value]").remove();
-			$("#id_metrado4").children("option[value]").remove();
-			var metrado2 = data["metrado2"];
-			for(var i=0;i<metrado2.length;i++){
-				var id = metrado2[i]["id"];
-				var descripcion = metrado2[i]["descripcion"]
-				var option = "<option value='"+id+"'>"+descripcion+"</option>"
-				$("#id_metrado2").append(option);
-				codigo_metrado2 = metrado2[i]["codigo"];
+	function addOptions(id_metrado,args){
+		if(args.length > 0){
+			var codigos = [];
+			$(id_metrado).append("<option value selected='selected'>---------</option>");
+			for(i in args){
+				var id = args[i]["id"];
+				var descripcion = args[i]["descripcion"];
+				var option = "<option value='"+id+"'>"+descripcion+"</option>";
+				$(id_metrado).append(option);
+				codigos.push({id: args[i]["codigo"]});
+			}
+			return codigos;
+		}
+	}
+	$("select").change(function(ev){
+		$(this).each(function(index){
+			var id_select = $(this).attr("id");
+			var value_select = $(this).val();
+			if(id_select.endsWith("metrado1")){
+				var url = "/metrado/?metrado1_id="+value_select;
+				$.getJSON(url,function(data){
+					var id_metrado2 = "#"+id_select.replace("metrado1","metrado2");
+					var id_metrado3 = "#"+id_select.replace("metrado1","metrado3");
+					var id_metrado4 = "#"+id_select.replace("metrado1","metrado4");
+					$(id_metrado2).children().remove();
+					$(id_metrado3).children().remove();
+					$(id_metrado4).children().remove();
+					addOptions(id_metrado2,data["metrado2"]);
+				});
+			}
+			else if(id_select.endsWith("metrado2")){
+				var url = "/metrado/?metrado2_id="+value_select;
+				$.getJSON(url,function(data){
+					var id_metrado3 = "#"+id_select.replace("metrado2","metrado3");
+					var id_metrado4 = "#"+id_select.replace("metrado2","metrado4");
+					$(id_metrado3).children().remove();
+					$(id_metrado4).children().remove();
+					addOptions(id_metrado3,data["metrado3"]);
+				});
+			}
+			else if(id_select.endsWith("metrado3")){
+				var url = "/metrado/?metrado3_id="+value_select;
+				$.getJSON(url,function(data){
+					var id_metrado4 = "#"+id_select.replace("metrado3","metrado4");
+					$(id_metrado4).children().remove();
+					addOptions(id_metrado4,data["metrado4"]);
+				});
 			}
 		});
 	});
-	$("#id_metrado2").change(function(ev){
-		var url = "/metrado/?metrado2_id="+$(this).val();
-		$.getJSON(url,function(data){
-			$("#id_metrado3").children("option[value]").remove();
-			$("#id_metrado4").children("option[value]").remove();
-			var metrado3 = data["metrado3"];
-			for(var i=0;i<metrado3.length;i++){
-				var id = metrado3[i]["id"];
-				var descripcion = metrado3[i]["descripcion"]
-				var option = "<option value='"+id+"'>"+descripcion+"</option>"
-				$("#id_metrado3").append(option);
-				codigo_metrado3 = metrado3[i]["codigo"];
-				if($("#id_metrado4").children().length > 1){
-					$("#id_metrado4").children().each(function(index){
-						if($(this).prop("selected"))
-							$(".td-partida").text(codigo_metrado2);
-					});
-				}
-				else if($("#id_metrado3").children().length > 1){
-					$("#id_metrado3").children().each(function(index){
-						if($(this).prop("selected"))
-							$(".td-partida").text(codigo_metrado3);
-					});
-				}
-				else{
-					$("#id_metrado2").children().each(function(index){
-						if($(this).prop("selected"))
-							$(".td-partida").text(codigo_metrado4);
-					});
-				}
-			}
-		});
-	});
-	$("#id_metrado3").change(function(ev){
-		var url = "/metrado/?metrado3_id="+$(this).val();
-		$.getJSON(url,function(data){
-			$("#id_metrado4").children("option[value]").remove();
-			var metrado4 = data["metrado4"];
-			for(var i=0;i<metrado4.length;i++){
-				var id = metrado4[i]["id"];
-				var descripcion = metrado4[i]["descripcion"]
-				var option = "<option value='"+id+"'>"+descripcion+"</option>"
-				$("#id_metrado4").append(option);
-				codigo_metrado4 = metrado4[i]["codigo"];
-			}
-		});
-	});
-	$("#agregar").click(function(ev){
-		$("#submit").show();
-		var val_numero = $("#id_numero").val();
-		var val_parcial = $("#id_parcial").val();
-		var total = val_numero*val_parcial;
-		var val_unidad = $("#id_unidad").val();
-		var val_punitario = $("#id_punitario").val();
-		var precio_total = val_unidad*val_punitario;
-		if($("#id_metrado4").children().length > 1){
-			$("#id_metrado4").children().each(function(index){
-				if($(this).prop("selected"))
-					$(".td-partida").text(codigo_metrado2);
-			});
+	var val_numero,val_parcial,val_unidad,val_punitario;
+	$("input").change(function(ev){
+		var id_input = $(this).attr("id");
+		var val_input = $(this).val();
+		if(id_input.endsWith("numero")){
+			var id_tabla = "#"+id_input.replace("numero","tabla");
+			val_numero = val_input;
+			$(id_tabla+" #td-numero-veces").text(val_input);
 		}
-		else if($("#id_metrado3").children().length > 1){
-			$("#id_metrado3").children().each(function(index){
-				if($(this).prop("selected"))
-					$(".td-partida").text(codigo_metrado3);
-			});
+		else if(id_input.endsWith("largo")){
+			var id_tabla = "#"+id_input.replace("largo","tabla");
+			$(id_tabla+" #td-dimensiones-largo").text(val_input);
 		}
-		else{
-			$("#id_metrado2").children().each(function(index){
-				if($(this).prop("selected"))
-					$(".td-partida").text(codigo_metrado4);
-			});
+		else if(id_input.endsWith("ancho")){
+			var id_tabla = "#"+id_input.replace("ancho","tabla");
+			$(id_tabla+" #td-dimensiones-ancho").text(val_input);
 		}
-		$(".td-numero-veces").text(val_numero);
-		$(".td-dimensiones-largo").text($("#id_largo").val());
-		$(".td-dimensiones-ancho").text($("#id_ancho").val());
-		$(".td-dimensiones-altura").text($("#id_alto").val());
-		$(".td-parcial").text(val_parcial);
-		$(".td-total").text(total);
-		$(".td-unidad").text(val_unidad);
-		$(".td-precio-unitario").text(val_punitario);
-		$(".td-precio-total").text(precio_total);
+		else if(id_input.endsWith("alto")){
+			var id_tabla = "#"+id_input.replace("alto","tabla");
+			$(id_tabla+" #td-dimensiones-altura").text(val_input);
+		}
+		else if(id_input.endsWith("parcial")){
+			var id_tabla = "#"+id_input.replace("parcial","tabla");
+			val_parcial = val_input;
+			$(id_tabla+" #td-parcial").text(val_input);
+		}
+		else if(id_input.endsWith("unidad")){
+			var id_tabla = "#"+id_input.replace("unidad","tabla");
+			val_unidad = val_input;
+			$(id_tabla+" #td-unidad").text(val_input);
+		}
+		else if(id_input.endsWith("punitario")){
+			var id_tabla = "#"+id_input.replace("punitario","tabla");
+			val_punitario = val_input;
+			$(id_tabla+" #td-precio-unitario").text(val_input);
+		}
+		
+		if(val_numero && val_parcial){
+			var total = val_numero*val_parcial;
+			$(id_tabla+" #td-total").text(total);
+		}
+		if(val_unidad && val_punitario){
+			var precio_total = val_unidad*val_punitario;
+			$(id_tabla+" #td-precio-total").text(precio_total);
+		}		
+		// if($("#id_metrado4").children().length > 1){
+		// 	$("#id_metrado4").children().each(function(index){
+		// 		if($(this).prop("selected"))
+		// 			$("#td-partida").text(codigo_metrado2);
+		// 	});
+		// }
+		// else if($("#id_metrado3").children().length > 1){
+		// 	$("#id_metrado3").children().each(function(index){
+		// 		if($(this).prop("selected"))
+		// 			$("#td-partida").text(codigo_metrado3);
+		// 	});
+		// }
+		// else{
+		// 	$("#id_metrado2").children().each(function(index){
+		// 		if($(this).prop("selected"))
+		// 			$("#td-partida").text(codigo_metrado4);
+		// 	});
+		// }
+		
 	});
-	$(".td-borrar").click(function(ev){
-		$("#submit").hide();
-		$(".td-partida").text("");
-		$(".td-numero-veces").text("");
-		$(".td-dimensiones-largo").text("");
-		$(".td-dimensiones-ancho").text("");
-		$(".td-dimensiones-altura").text("");
-		$(".td-parcial").text("");
-		$(".td-total").text("");
-		$(".td-unidad").text("");
-		$(".td-precio-unitario").text("");
-		$(".td-precio-total").text("");
-	});
-	// $("#ficha-tecnica-form").submit(function(ev){
-	// 	return ev.preventDefault();
-	// });
 });
