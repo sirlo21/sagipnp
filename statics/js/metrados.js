7143682fe278757176d3055be2b9b9473bd3c832
Ruben <ruben.ignaciogz@gmail.com>
2015-09-25 15:55:00 -0500

    change metrados.js

--- statics/js/metrados.js
+++ statics/js/metrados.js
@@ -20,13 +20,10 @@ $(document).ready(function(){
 		}
 		return codigos;
 	}
-	function rollback(url,id_m){
-		$.getJSON(url,function(data){
-			var back = data["back"]
-			var m = $(id_m).children("option[value="+back["id"]+"]");
-			m.attr({selected: ""});
-		});
-	}
+	$("datalist").change(function(event){
+		alert("Hola");
+		event.preventDefault();
+	});
 	var codigo_metrado2,codigo_metrado3,codigo_metrado4;
 	$("select").change(function(ev){
 		var id_select = $(this).attr("id");
@@ -58,15 +55,12 @@ $(document).ready(function(){
 			$.getJSON(url,function(data){
 				var id_tabla = "#"+id_select.replace("metrado2","tabla");
 				var id_metrado1 = "#"+id_select.replace("metrado2","metrado1");
-
 				var id_metrado3 = "#"+id_select.replace("metrado2","metrado3");
 				var id_metrado4 = "#"+id_select.replace("metrado2","metrado4");
 				$(id_metrado3).children().remove();
 				$(id_metrado4).children().remove();
 				addOptions(id_metrado3,data["metrado3"]);
 				codigo_metrado3 = getCodigosMetrado(data["metrado3"]);
-				var url = "/metrado/?rollback_m1="+value_select;
-				rollback(url,id_metrado1);
 				if($(id_metrado3).children().length > 1){
 					if($(id_metrado3).val()){
 						$(id_metrado3).children().each(function(index){
@@ -91,11 +85,6 @@ $(document).ready(function(){
 				$(id_metrado4).children().remove();
 				addOptions(id_metrado4,data["metrado4"]);
 				codigo_metrado4 = getCodigosMetrado(data["metrado4"]);
-				var url = "/metrado/?rollback_m2="+value_select;
-				rollback(url,id_metrado2);
-				var m2 = $(id_metrado2);
-				url = "/metrado/?rollback_m1="+m2.val();
-				rollback(url,id_metrado1);
 				if($(id_metrado4).children().length > 1){
 					if($(id_metrado4).val()){
 						$(id_metrado4).children().each(function(index){
@@ -112,17 +101,6 @@ $(document).ready(function(){
 		}
 		else if(id_select.endsWith("metrado4")){
 			var id_tabla = "#"+id_select.replace("metrado4","tabla");
-			var id_metrado1 = "#"+id_select.replace("metrado4","metrado1");
-			var id_metrado2 = "#"+id_select.replace("metrado4","metrado2");
-			var id_metrado3 = "#"+id_select.replace("metrado4","metrado3");
-			var url = "/metrado/?rollback_m3="+value_select;
-			rollback(url,id_metrado3,value_select);
-			var m1 = $(id_metrado1);
-			var url = "/metrado/?rollback_m2="+m1.val();
-			rollback(url,id_metrado2);
-			var m2 = $(id_metrado2);
-			url = "/metrado/?rollback_m1="+m2.val();
-				rollback(url,id_metrado1);
 			if($(this).children().length > 1){
 				if(value_select)
 					$(id_tabla+" #td-partida").text(codigo_metrado4[value_select]);
