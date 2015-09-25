54d3678ea2e23970e0462e2fa4ee1a40172fc9bd
Ruben <ruben.ignaciogz@gmail.com>
2015-09-25 15:57:34 -0500

    change models.py

--- metrados/models.py
+++ metrados/models.py
@@ -34,7 +34,6 @@ class Metrado4(models.Model):
 
 class FichaTecnica(models.Model):
 	form  = models.ForeignKey(Levantamiento,related_name='ficha_tecnica')
-	nombre = models.CharField(max_length=500)
 	metrado1 = models.ForeignKey(Metrado1,related_name='ficha_tecnica')
 	metrado2 = models.ForeignKey(Metrado2,related_name='ficha_tecnica')
 	metrado3 = models.ForeignKey(Metrado3,related_name='ficha_tecnica',null=True,blank=True)
