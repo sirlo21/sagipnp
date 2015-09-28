36901ae2561ae115cbe8f7400e9e394a289fd5ec
Ruben <ruben.ignaciogz@gmail.com>
2015-09-28 12:20:26 -0500

    change urls.py

--- levantamiento/urls.py
+++ levantamiento/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
 	url(r"^$",views.index,name="index"),
 	url(r"^equipo-de-levantamiento/$",views.equipo_de_levantamiento,name="equipo_de_levantamiento"),
 	url(r"^acciones-de-prevencion/(?P<id>\d+)/$",views.acciones_de_prevencion,name="acciones_de_prevencion"),
-	url(r"^json/$",views.json,name="json"),
+	url(r"^equipo-de-levantamiento/json/$",views.json,name="levantamiento_json"),
 )
