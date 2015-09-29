c0acba0e3a37a3b8b8d208736f2d591ee73c2b39
Ruben <ruben.ignaciogz@gmail.com>
2015-09-29 15:10:28 -0500

    change metrados.js

--- statics/js/metrados.js
+++ statics/js/metrados.js
@@ -1,31 +1,39 @@
-$(document).ready(function(){
-	function addOptions(id_metrado,args,rollback){
-		if(args.length > 0){
-			for(i in args){
-				var id = args[i]["id"];
-				var descripcion = args[i]["descripcion"];
-				if(rollback){
-					if(id == rollback)
-						var option = "<option value='"+id+"' selected>"+descripcion+"</option>";
-					else
-						var option = "<option value='"+id+"'>"+descripcion+"</option>";
-				}
+var metrados = {};
+function addOptions(id_metrado,args,rollback){
+	if(args.length > 0){
+		for(i in args){
+			var id = args[i]["id"];
+			var descripcion = args[i]["descripcion"];
+			if(rollback){
+				if(id == rollback)
+					var option = "<option value='"+id+"' selected>"+descripcion+"</option>";
 				else
 					var option = "<option value='"+id+"'>"+descripcion+"</option>";
-				$(id_metrado).append(option);
 			}
+			else
+				var option = "<option value='"+id+"'>"+descripcion+"</option>";
+			$(id_metrado).append(option);
 		}
 	}
-	function getCodigosMetrado(args){
-		var codigos = {};
-		if(args.length > 0){
-			for(i in args){
-				var id = args[i]["id"];
-				codigos[id] = args[i]["codigo"];
-			}
+}
+function getCodigosMetrado(args){
+	var codigos = {};
+	if(args.length > 0){
+		for(i in args){
+			var id = args[i]["id"];
+			codigos[id] = args[i]["codigo"];
 		}
-		return codigos;
 	}
+	return codigos;
+}
+function removeTr(id){
+	if(confirm("¿Estas seguro(a) de querer borrar esta fila?")){
+		if(id in metrados)
+			metrados[id] = undefined;
+		$("#"+id).remove();
+	}
+}
+$(document).ready(function(){
 	var codigo_metrado2,codigo_metrado3,codigo_metrado4;
 	$("#id_buscar").click(function(event){
 		var input_nombre_id = "#id_nombre";
@@ -78,7 +86,8 @@ $(document).ready(function(){
 		var id_metrado2 = "#id_metrado2";
 		var id_metrado3 = "#id_metrado3";
 		var id_metrado4 = "#id_metrado4";
-		var tr = "#"+$("#tm > tbody tr").last().attr("id");
+		var tr = $("#tm > tbody tr").last().children(".td-partida");
+		var tr_id = $("#tm > tbody tr").last().attr("id");
 		if(id_select == id_metrado1){
 			$(id_metrado2).children("option[value]").remove();
 			$(id_metrado3).children("option[value]").remove();
@@ -100,19 +109,19 @@ $(document).ready(function(){
 					if($(id_metrado3).val()){
 						$(id_metrado3).children().each(function(index){
 							if($(this).prop("selected"))
-								$(tr+" #td-partida").text(codigo_metrado3[$(this).val()]);
+								tr.text(codigo_metrado3[$(this).val()]);
 						});
 					}
 				}
 				else{
 					if(value_select)
-						$(tr+" #td-partida").text(codigo_metrado2[value_select]);
+						tr.text(codigo_metrado2[value_select]);
 				}
 			});
 		}
 		else if(id_select == id_metrado3){
-			var url = "/metrado/json/?metrado3_id="+value_select;
 			$(id_metrado4).children("option[value]").remove();
+			var url = "/metrado/json/?metrado3_id="+value_select;
 			$.getJSON(url,function(data){
 				addOptions(id_metrado4,data["metrado4"]);
 				codigo_metrado4 = getCodigosMetrado(data["metrado4"]);
@@ -120,53 +129,75 @@ $(document).ready(function(){
 					if($(id_metrado4).val()){
 						$(id_metrado4).children().each(function(index){
 							if($(this).prop("selected"))
-								$(tr+" #td-partida").text(codigo_metrado4[$(this).val()]);
+								tr.text(codigo_metrado4[$(this).val()]);
 						});
 					}
 				}
 				else{
 					if(value_select)
-						$(tr+" #td-partida").text(codigo_metrado3[value_select]);
+						tr.text(codigo_metrado3[value_select]);
 				}
 			});
 		}
 		else if(id_select == id_metrado4){
 			if($(this).children().length > 1){
 				if(value_select)
-					$(tr+" #td-partida").text(codigo_metrado4[value_select]);
+					tr.text(codigo_metrado4[value_select]);
 			}
 		}
+		metrados[tr_id] = $("#ficha-tecnica-form").serialize();
 		$("input").trigger("change");
 	});
-	$("#plus").click(function(ev){
-		$("#tm > tbody tr").last();
+	$("#add").click(function(ev){
+		var tr_id = $("#tm > tbody > tr").last().attr("id");
+		if($("#tm > tbody > tr").length > 0)
+			var new_tr_id = tr_id.replace(/\d+/g,parseInt(tr_id.match(/\d+/g))+1);
+		else
+			var new_tr_id = "id_ficha_tecnica-0-tr";
+		new_tr_id_remove = new_tr_id.replace("tr","remove");
+		var new_tr = '<tr id="'+new_tr_id+'">\n';
+		new_tr += '<td class="td-partida"></td>\n';
+		new_tr += '<td class="td-numero-veces"></td>\n';
+		new_tr += '<td class="td-dimensiones-largo"></td>\n';
+		new_tr += '<td class="td-dimensiones-ancho"></td>\n';
+		new_tr += '<td class="td-dimensiones-altura"></td>\n';
+		new_tr += '<td class="td-parcial"></td>\n';
+		new_tr += '<td class="td-total"></td>\n';
+		new_tr += '<td class="td-unidad"></td>\n';
+		new_tr += '<td class="td-precio-unitario"></td>\n';
+		new_tr += '<td class="td-precio-total"></td>\n';
+		new_tr += '<td>\n<button type="button" id="'+new_tr_id_remove+'" onclick="removeTr(\''+new_tr_id+'\');" class="remove btn btn-danger">Borrar</button>\n</td>\n';
+		new_tr += '</tr>';
+		$("#tm > tbody").append($(new_tr));
 	});
 	$("input").change(function(ev){
-		var tr = $("#tm > tbody > tr").last().children;
+		var tr = $("#tm > tbody > tr").last();
+		var tr_id = tr.attr("id");
 		var $numero = $("#id_numero");
 		var $val_numero = $numero.val();
-		$("#tm > tbody > tr #td-numero-veces").text($val_numero);
+		tr.children(".td-numero-veces").text($val_numero);
 		var $largo = $("#id_largo");
 		var $val_largo = $largo.val();
-		$("#tm > tbody > tr #td-dimensiones-largo").text($val_largo);
+		tr.children(".td-dimensiones-largo").text($val_largo);
 		var $ancho = $("#id_ancho");
 		var $val_ancho = $ancho.val();
-		$("#tm > tbody > tr #td-dimensiones-ancho").text($val_ancho);
+		tr.children(".td-dimensiones-ancho").text($val_ancho);
 		var $alto = $("#id_alto");
 		var $val_alto = $alto.val();
-		$("#tm > tbody > tr #td-dimensiones-altura").text($val_alto);
+		tr.children(".td-dimensiones-altura").text($val_alto);
 		var $parcial = $("#id_parcial");
 		var $val_parcial = $parcial.val();
-		$("#tm > tbody > tr #td-parcial").text($val_parcial);
+		tr.children(".td-parcial").text($val_parcial);
 		var $unidad = $("#id_unidad");
 		var $val_unidad = $unidad.val();
-		$("#tm > tbody > tr #td-unidad").text($val_unidad);
+		tr.children(".td-unidad").text($val_unidad);
 		var $punitario = $("#id_punitario");
 		var $val_punitario = $punitario.val();
-		$("#tm > tbody > tr #td-precio-unitario").text($val_punitario);
+		tr.children(".td-precio-unitario").text($val_punitario);
 		var total = $val_numero*$val_parcial;
-		$("#tm > tbody > tr #td-total").text(total);
+		tr.children(".td-total").text(total);
 		var precio_total = $val_unidad*$val_punitario;
-		$("#tm > tbody > tr #td-precio-total").text(precio_total);
+		tr.children(".td-precio-total").text(precio_total);
+		metrados[tr_id] = $("#ficha-tecnica-form").serialize();
 	});
 });
