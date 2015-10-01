b814a3702e3647a3413c9afb549503a2e6c8131f
Ruben <ruben.ignaciogz@gmail.com>
2015-10-01 14:55:27 -0500

    change metrados.js

--- statics/js/metrados.js
+++ statics/js/metrados.js
@@ -248,7 +248,7 @@ $(document).ready(function(){
 									type: "POST",
 									done: function(){
 										if(arr.length-1 == i)
-											location.href = "/";
+											console.log(location.href = "/");
 									}
 								});
 							}
