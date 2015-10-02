c7b806396698c0ef6bd21254b5ffd39e02e64369
Ruben <ruben.ignaciogz@gmail.com>
2015-10-02 16:24:00 -0500

    change forms.py

--- metrados/forms.py
+++ metrados/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 from metrados.models import *
 from django.forms import inlineformset_factory
-from levantamiento.models import Levantamiento
+from levantamiento.models import Levantamiento,Instalacion
+from ubigeo.fields import UbigeoField
+from ubigeo.models import Ubigeo
 
 class FichaTecnicaForm(forms.ModelForm):
 	class Meta:
@@ -23,3 +25,22 @@ class FichaTecnicaForm(forms.ModelForm):
 			"unidad": forms.NumberInput(attrs={"class": "form-control","min": 0,"value": 0}),
 			"punitario": forms.NumberInput(attrs={"class": "form-control","min": 0,"value": 0})
 		}
+
+class UbigeoForm(forms.Form):
+	CONSULTA_CHOICES = ((1,u"Consulta financiera de las reparaciones"),
+		(2,u"Consulta de Numero y relación de Instalaciones intervenidas"),
+		(3,u"Estado de las acciones de prevención")
+	)
+	TIPO_INSTALACION_CHOICES = []
+	for instalacion in Instalacion.objects.all():
+		TIPO_INSTALACION_CHOICES.append((instalacion.id,instalacion.instalacion))
+	ubigeo = UbigeoField(
+		attrs_1={"class": "form-control", "style": "width: 30%; display: inline;"},
+		attrs_2={"class": "form-control", "style": "width: 35%; display: inline;"},
+		attrs_3={"class": "form-control", "style": "width: 35%; display: inline;"}
+	)
+	tipo_instalacion = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-control"}),choices=TIPO_INSTALACION_CHOICES)
+	consulta = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-control"}),choices=CONSULTA_CHOICES)
+
+	class Media:
+		js = ('js/ubigeo.js','js/select_ubigeo.js')
