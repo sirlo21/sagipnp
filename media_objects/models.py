0aa200fb12f1c6cf3fa10ce7d5c872c847e478f9
Ruben <ruben.ignaciogz@gmail.com>
2015-10-09 17:13:25 -0500

    change models.py

--- media_objects/models.py
+++ media_objects/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-class Image(models.Model):
+class Media(models.Model):
+	content_type = models.ForeignKey(ContentType)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey('content_type', 'object_id')
+
+class Image(Media):
 	img = models.ImageField(upload_to="images/")
 
-class Document(models.Model):
+class Document(Media):
 	doc	= models.FileField(upload_to="docs/")
