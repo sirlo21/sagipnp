0a4d618ff8a32fa7b101399e58e6bd1f334f43fc
Ruben <ruben.ignaciogz@gmail.com>
2015-10-13 15:57:55 -0500

    change urls.py

--- metrados/urls.py
+++ metrados/urls.py
@@ -4,5 +4,6 @@ from metrados import views
 urlpatterns = patterns('',
 	url(r"^ficha-tecnica/(?P<id>\d+)/$",views.ficha_tecnica,name="ficha_tecnica"),
 	url(r"^reportes/$",views.reportes,name="reportes"),
+	url(r"^reportes/(?P<tipo_instalacion>\w+(-\w+)*)/$",views.reporte_instalacion,name="reporte_instalacion"),
 	url(r"^metrado/json/$",views.json,name="metrado_json"),
 )
