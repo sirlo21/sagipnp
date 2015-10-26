1fe6790068167502f9bf4a410badc9a3d947f2a7
Ruben <ruben.ignaciogz@gmail.com>
2015-10-26 12:33:34 -0500

    change levantamiento/admin.py

--- levantamiento/admin.py
+++ levantamiento/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from levantamiento.models import *
 
-# Register your models here.
+admin.site.register(TComisaria)
+admin.site.register(CComisaria)
+admin.site.register(Especialidad)
+admin.site.register(Category)
+admin.site.register(SituacionLegal)
+admin.site.register(RegionPolicial)
+admin.site.register(DivisionPolicial)
+admin.site.register(Grado)
+admin.site.register(UnidadEjecutora)
+admin.site.register(Instalacion)
+admin.site.register(Levantamiento)
