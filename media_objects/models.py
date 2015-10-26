d723fee111a8f398242625766fbd5276d3699648
Ruben <ruben.ignaciogz@gmail.com>
2015-10-26 12:33:10 -0500

    change media_objects/models.py

--- media_objects/models.py
+++ media_objects/models.py
@@ -1,11 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 class Media(models.Model):
+	description = models.CharField("Descripción", max_length=400, blank=True)
+	date_registration = models.DateTimeField(auto_now_add=True)
 	content_type = models.ForeignKey(ContentType)
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
+
+	class Meta:
+		abstract = True
 
 class Image(Media):
 	img = models.ImageField(upload_to="images/")
