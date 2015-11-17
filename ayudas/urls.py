6751dffa08d91c1cc8d5272ed1e6dcd972f69d48
Ruben <ruben.ignaciogz@gmail.com>
2015-11-17 14:04:28 -0500

    change

--- ayudas/urls.py
+++ ayudas/urls.py
@@ -5,6 +5,4 @@ urlpatterns = patterns('',
 	url(r"^$",views.index,name="ayuda_index"),
 	url(r"^edit/(?P<form>\w+(.\w)*)$",views.edit,name="ayuda_edit"),
 	url(r"^show/(?P<form>\w+(.\w)*)/(?P<posicion>\d+)/$",views.show,name="show_ayuda"),
-	url(r"^update/(?P<form>\w+(.\w)*)$",views.update,name="ayuda_update"),
-	url(r"^create/(?P<form>\w+(.\w)*)$",views.create,name="ayuda_create"),
 )
--- statics/js/ayudas.js
+++ statics/js/ayudas.js
@@ -1,51 +1,20 @@
-var summernote = function(){
-	$('.summernote').summernote({
-		height: 200,
-		tabsize: 2,
-		codemirror: {theme: 'monokai'}
-	});
+function delete_row(){
+	if($("#ayudas_form").children(".ayuda").length <= 0){
+		$(".add-row").prepend($("<h2 id='vacio'>").text("No hay ninguna ayuda"));
+	}
 }
 $(document).ready(function(){
-	var submit = true;
-	$("#form_ayuda").submit(function(event){
-		$(".guardado").remove();
-		$(".error").remove();
-		if(submit){
-			$.ajax({
-				url: $(this).attr("action"),
-				data: $(this).serialize(),
-				type: $(this).attr("method"),
-				success: function(data){
-					$(".submit").after("<span class='guardado'>&nbsp;Guardado!</span>");
-				},	
-				error: function(data){
-					$(".submit").after("<span class='error'>&nbsp;Error al guardado!</span>")
-				}
-			});
-		}
-		return event.preventDefault();
-	});
-	$("#create").click(function(event){
-		$.ajax({
-			url: $(this).attr("href"),
-			type: "GET",
-			success: function(data){
-				var ayuda = data["ayuda"];
-				var fields = "<li style='font-weight: bold;'><label for='title'>Titulo: </label></li>";
-				fields += "<input class='form-control' id='title' name='title["+ayuda["id"]+"]' maxlength='40' type='text' value='"+ayuda["title"]+"' /><br/>";
-				fields += "<label for='posicion'>Posicion: </label>";
-				fields += "<input class='form-control' id='posicion' name='posicion["+ayuda["id"]+"]' type='number' value='"+ayuda["posicion"]+"' /><br/>";
-				fields += "<label for='text'>Texto:</label><br/>";
-				fields += "<textarea id='text["+ayuda["id"]+"]' type='text' class='summernote' name='text["+ayuda["id"]+"]' placeholder='Texto de ayuda'></textarea>";
-				if($("#vacio").length > 0){
-					$("#vacio").before("<input class='btn btn-success submit' id='submit1' type='submit' value='Guardar' /><br/>");
-					$("#vacio").remove();
-					$("#submit1").after("<input class='btn btn-success submit' id='submit2' type='submit' value='Guardar' /><br/>");
-				}
-				$("#submit2").before("<div class='form-group'>"+fields+"</div><br/>");
-				summernote();
-			}
+	$(".delete-row").ready(function(){
+		$.each($(".delete-row"),function(key,value){
+			$(this).attr({onclick: "delete_row();"});
 		});
-		return event.preventDefault();
+	});
+	$(".add-row").ready(function(){
+		$(".add-row").click(function(event){
+			$("#vacio").remove();
+			$.each($(".delete-row"),function(key,value){
+				$(this).attr({onclick: "delete_row();"});
+			});
+		});
 	});
 });
