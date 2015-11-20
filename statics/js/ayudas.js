56ca9e25c91db8ef1b8873c07cd9725911e0d7da
Ruben <ruben.ignaciogz@gmail.com>
2015-11-20 15:56:17 -0500

    change statics/js/ayudas.js

--- statics/js/ayudas.js
+++ statics/js/ayudas.js
@@ -3,7 +3,15 @@ function delete_row(){
 		$(".add-row").prepend($("<h2 id='vacio'>").text("No hay ninguna ayuda"));
 	}
 }
+var summernote = function(){
+	$('.summernote').summernote({
+		height: 200,
+		tabsize: 2,
+		codemirror: {theme: 'monokai'}
+	});
+}
 $(document).ready(function(){
+	summernote();
 	$(".delete-row").ready(function(){
 		$.each($(".delete-row"),function(key,value){
 			$(this).attr({onclick: "delete_row();"});
